e09864c7fcfecd0871f3051697258295f8a0ddd2
“Pavel <itspavelnosarev@gmail.com>
2021-06-18 10:49:00 -0400

    added routes and controller

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+  #self.abstract_class = true
+  def movie_first
+    movie = Movies.first
+    render json: movie.as_json
+  end
+
+  def movie_all
+    movie = Movies.all
+    render json: movie.as_json
+  end
 end
