9c3eb7061d11680cc4d77632a16dd87e5064a4e1
“Pavel <itspavelnosarev@gmail.com>
2021-06-22 17:52:14 -0400

    added RESTful

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -23,4 +23,38 @@ class MoviesController < ApplicationController
     input_value = params["actor"]
     render json: { message: "#{input_value}." }
   end
+
+  def index
+    movie = Movie.all
+    render json: movie.as_json
+  end
+  def create
+    movie = Movie.new(
+      first_name: params["first_name"]
+      last_name: params["last_name"]
+      known_for: params["known_for"]
+    )
+     movie.save
+     render json: movie.as_json
+  end
+  def show
+    movie_id = params["id"]
+    movie = Movie.find_by(id: movie_id)
+    render json: movie
+    end
+  def update
+    movie_id = params["id"]
+    movie = Movie.find(id: movie_id)
+    movie.first_name = params["first_name"] || product.first_name
+    product.last_name = params["last_name"] || product.last_name
+    product.known_for = params["known_for"] || product.known_for
+    product.save
+    render json: movie.as_json
+  end
+  def destroy
+    movie_id = params[:id]
+    movie = movie.find(id:movie_id)
+    movie.destroy
+    render json: {message: "This has been destroyed!"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # get "/get_id", controller: id, action: "get_id"
-  get "/movie_first" => "movies#movie_first"
-  get "/movie_all" => "movies#movie_all"
-  get "/query_params" => "movies#query_params"
-  get "/segment_params/:id" => "movies#segment_params"
-  post "/body_params" => "movies#body_params"
+  # get "/movie_first" => "movies#movie_first"
+  # get "/movie_all" => "movies#movie_all"
+  # get "/query_params" => "movies#query_params"
+  # get "/segment_params/:id" => "movies#segment_params"
+  # post "/body_params" => "movies#body_params"
+  get "/movies" => "movies#index"
+  post "/movies" => "movies#create"
+  get "/movies/:id" => "movies#show"
+  patch "/movies/:id" => "movies#update"
+  delete "/movies/:id" => "movies#destroy"
 end
