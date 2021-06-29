02f6b868eb4cd94b0949aa2b3f2c580366d469b2
“Pavel <itspavelnosarev@gmail.com>
2021-06-29 18:31:48 -0400

    added serializer for actor and movie

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,17 @@ class ActorsController < ApplicationController
     actors = Actor.all.order(:age :desc :english :gender)
     render json: actors.as_json
   end
+  def create
+    actors = Actors.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      known_for: params["known_for"],
+      age: params
+      actor_age:
+      actor_gender:
+      movie_id:
+    
+    
+    
+    )
 end
