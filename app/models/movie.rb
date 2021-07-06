c05f9b409da3acde35df5f9500cff04f2a453deb
“Pavel <itspavelnosarev@gmail.com>
2021-07-06 17:48:27 -0400

    aDDED TO SERIALIZER AND ADDEDNEW MODELS

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   has_many :actors
   # validates :director, presence: true
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # Character.create(name: "Luke", movie: movies.first)
-Actor.create!(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
-Actor.create!({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
-Actor.create!({ first_name: "Tom", last_name: "Hanks", known_for: "Castaway" })
+# Actor.create!(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
+# Actor.create!({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+# Actor.create!({ first_name: "Tom", last_name: "Hanks", known_for: "Castaway" })
 
-Actor.create!({ first_name: "Tom", last_name: "Cruise", known_for: "Mission Impossible" })
-Actor.create!({ first_name: "Johhny", last_name: "Depp", known_for: "Black Pearl" })
+# Actor.create!({ first_name: "Tom", last_name: "Cruise", known_for: "Mission Impossible" })
+# Actor.create!({ first_name: "Johhny", last_name: "Depp", known_for: "Black Pearl" })
+
+# movie = Movie.create!(title: "Castaway", director: "Robert Zemeckis
+#   ")
+# movie = Movie.create!(title: "Mission Impossible", director: "Brian De Palma")
+# movie = Movie.create!(title: "Black Pearl", director: "Gore Verbinski")
+
+# genre = Genre.create!(name: "Action")
+# genre = Genre.create!(name: "Family")
+# genre = Genre.create!(name: "Drama")
+
+# MovieGenre.create!(movie_id: 1, genre_id: 3)
+# MovieGenre.create!(movie_id: 2, genre_id: 1)
+# MovieGenre.create!(movie_id: 3, genre_id: 2)
